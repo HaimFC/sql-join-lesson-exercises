1a77ba02a3e292f6e166d6fe49cdb4966444cea0
HaimFC <haimtehem@gmail.com>
2025-09-01 09:37:11 +0300

    spot check 2 sol

--- solutions/check_2.sql
+++ solutions/check_2.sql
@@ -1,1 +1,15 @@
-USE sql_intro;
+USE sql_join;
+
+
+SELECT 
+    item_purchased, amount, industry,
+    cu.name AS cust_name,
+    co.name AS comp_name
+FROM 
+    transaction AS tr,
+    customer AS cu,
+    company AS co
+    
+WHERE 
+    tr.company_id = co.id AND
+    tr.customer_id = cu.id;
