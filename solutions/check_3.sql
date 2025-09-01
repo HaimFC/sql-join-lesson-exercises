79d762e9c0a01ce181c67945bb155d880a426e53
HaimFC <haimtehem@gmail.com>
2025-09-01 09:55:16 +0300

    spot check 3 sol

--- solutions/check_3.sql
+++ solutions/check_3.sql
@@ -1,1 +1,37 @@
-USE sql_intro;
+USE sql_join;
+
+CREATE TABLE student(
+    s_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    s_name VARCHAR(20),
+    is_brilliant BOOLEAN
+);
+
+CREATE TABLE teacher(
+    t_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    t_name VARCHAR(80),
+    is_tenured  BOOLEAN
+);
+
+CREATE TABLE student_teacher(
+    student_id INT,
+    teacher_id INT,
+    FOREIGN KEY(student_id) REFERENCES student(s_id),
+    FOREIGN KEY(teacher_id) REFERENCES teacher(t_id)
+);
+
+INSERT INTO Student VALUES (1, 'Ryan', 1); 
+INSERT INTO Student VALUES (2, 'Leo', 1); 
+INSERT INTO Student VALUES (3, 'Ernie', 0); 
+
+INSERT INTO Teacher VALUES (1, 'Levine', 1);
+INSERT INTO Teacher VALUES (2, 'Foster', 0);
+INSERT INTO Teacher VALUES (3, 'Schwimmer', 0);
+
+INSERT INTO student_teacher (student_id, teacher_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (3, 1);
